777035eb26c5ac4066425197cb93fee1e4ebc40a
Jack Harvey <53987140+Jack-harvey@users.noreply.github.com>
2025-03-16 10:21:26 +1100

    Create gameboard object

--- scripts/script.js
+++ scripts/script.js
@@ -15,3 +15,75 @@ function createPlayer(name, boardPiece) {
     getScore,
   };
 }
+
+const gameBoardSquare = (function () {
+  let arrayOfSquares = [];
+  for (i = 0; i < 9; i++) {
+    switch (i) {
+      case 0:
+        arrayOfSquares.push({
+          position: { number: 0, row: 1, col: 1 },
+          mark: null,
+        });
+        break;
+      case 1:
+        arrayOfSquares.push({
+          position: { number: 1, row: 1, col: 2 },
+          mark: null,
+        });
+        break;
+      case 2:
+        arrayOfSquares.push({
+          position: { number: 2, row: 1, col: 3 },
+          mark: null,
+        });
+        break;
+      case 3:
+        arrayOfSquares.push({
+          position: { number: 3, row: 2, col: 1 },
+          mark: null,
+        });
+        break;
+      case 4:
+        arrayOfSquares.push({
+          position: { number: 4, row: 2, col: 2 },
+          mark: null,
+        });
+        break;
+      case 5:
+        arrayOfSquares.push({
+          position: { number: 5, row: 2, col: 3 },
+          mark: null,
+        });
+        break;
+      case 6:
+        arrayOfSquares.push({
+          position: { number: 6, row: 3, col: 1 },
+          mark: null,
+        });
+        break;
+      case 7:
+        arrayOfSquares.push({
+          position: { number: 7, row: 3, col: 2 },
+          mark: null,
+        });
+        break;
+      case 8:
+        arrayOfSquares.push({
+          position: { number: 8, row: 3, col: 3 },
+          mark: null,
+        });
+        break;
+    }
+  }
+  return arrayOfSquares;
+})();
+
+const gameLogic = (function () {
+  const isPlayersTurn = false;
+  const swapPlayersTurn = () => (isPlayersTurn = !isPlayersTurn);
+})();
+
+const gameBoard = (function () {
+  const gameGrid = [];
+})();
