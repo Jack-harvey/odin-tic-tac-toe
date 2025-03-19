b435d4ae4f35cbc2345d5938449d07d5c023e723
Jack Harvey <53987140+Jack-harvey@users.noreply.github.com>
2025-03-19 19:31:52 +1100

    Let results be printed to the console

--- scripts/script.js
+++ scripts/script.js
@@ -89,7 +89,7 @@ const gameBoard = (function () {
   };
 
   const getAllSquaresWithDefinedMark = (mark) => {
-    board.filter((square) => square.mark == mark);
+    return board.filter((square) => square.mark == mark);
   };
 
   const getEmptySquares = () => {
@@ -102,8 +102,119 @@ const gameBoard = (function () {
     return allFreeSquares.length;
   };
 
+  const printBoardToConsole = () => {
+    const map = board.map((square) =>
+      square.mark === null ? (square.consoleMark = "□") : (square.consoleMark = square.mark)
+    );
+    console.log(`${map[0]}${map[1]}${map[2]}`);
+    console.log(`${map[3]}${map[4]}${map[5]}`);
+    console.log(`${map[6]}${map[7]}${map[8]}`);
+  };
+
+  const test = () => {
+    return board;
+  };
+
+  return {
+    setMarker,
+    getAllSquaresWithDefinedMark,
+    getNumberOfEmptySquares,
+    validateSquareIsFree,
+    getEmptySquares,
+    test,
+    printBoardToConsole,
+  };
+})();
+
+const gameController = (function () {
+  let players = [];
+  let currentPlayerTurn = players[0];
+
+  const setupGame = () => {
+    const name = prompt("Pick a name");
+    const mark = prompt("Pick a token of X or O");
+    players.push(createPlayer(name, mark));
+    players[0].increaseTurnNumber();
+    players[0].increaseTurnNumber();
+    players[0].increaseTurnNumber();
+    players.push(createPlayer("Player-2", players[0].boardPiece == "X" ? "O" : "X"));
+    currentPlayerTurn = players[0];
+    gameBoard.printBoardToConsole();
+  };
+
+  const startTurn = (promptMessage = "Choose a sq from 0-8") => {
+    if (currentPlayerTurn == players[0]) {
+      playersChoice = prompt(promptMessage);
+    } else {
+      playersChoice = opponentController.opponentsMove();
+    }
+
+    if (!gameBoard.validateSquareIsFree(playersChoice)) {
+      startTurn("That square is already taken");
+    }
+
+    gameBoard.setMarker(playersChoice, currentPlayerTurn.boardPiece);
+    gameBoard.printBoardToConsole();
+    let winResult = evaluateWin(currentPlayerTurn);
+    evaluateGameEnd(winResult);
+  };
+
+  const evaluateWin = (player) => {
+    const isItTheLastTurn = gameBoard.getNumberOfEmptySquares == 0 ? true : false;
+    const marker = player.boardPiece;
+
+    if (player.getTurnNumber() < 3) {
+      return "";
+    }
+
+    if (confirmDiagonalMarkedSquares(marker)) {
+      return "win";
+    }
+
+    if (confirmStraightMarkedSquares(marker)) {
+      return "win";
+    }
+
+    if (isItTheLastTurn) {
+      return "draw";
+    } else {
+      return "";
+    }
+  };
+
+  const evaluateGameEnd = (winResult) => {
+    if (winResult == "") {
+      currentPlayerTurn = currentPlayerTurn === players[0] ? players[1] : players[0];
+      startTurn();
+    } else {
+      alert(`${winResult}`);
+    }
+  };
+
+  const confirmStraightMarkedSquares = (mark) => {
+    const NUMBEROFSQUARESTOWIN = 3;
+    const getAllSquaresWithDefinedMark = gameBoard.getAllSquaresWithDefinedMark(mark);
+
+    for (let index = 1; index < 4; index++) {
+      let numberOfMarksInRow = getAllSquaresWithDefinedMark.filter(
+        (square) => square.position.row == index
+      );
+      let numberOfMarksInCol = getAllSquaresWithDefinedMark.filter(
+        (square) => square.position.col == index
+      );
+
+      if (
+        numberOfMarksInCol.length == NUMBEROFSQUARESTOWIN ||
+        numberOfMarksInRow.length == NUMBEROFSQUARESTOWIN
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+
   const confirmDiagonalMarkedSquares = (mark) => {
-    const getAllSquaresWithDefinedMark = board.filter((square) => square.mark == mark);
+    const getAllSquaresWithDefinedMark = gameBoard.getAllSquaresWithDefinedMark(mark);
 
     if (
       getAllSquaresWithDefinedMark.findIndex(
@@ -131,163 +242,11 @@ const gameBoard = (function () {
     return false;
   };
 
-  const confirmStraightMarkedSquares = (mark) => {
-    const NUMBEROFSQUARESTOWIN = 3;
-    const getAllSquaresWithDefinedMark = board.filter((square) => square.mark == mark);
-
-    for (let index = 1; index < 4; index++) {
-      let numberOfMarksInRow = getAllSquaresWithDefinedMark.filter(
-        (square) => square.position.row == index
-      );
-      let numberOfMarksInCol = getAllSquaresWithDefinedMark.filter(
-        (square) => square.position.col == index
-      );
-
-      if (
-        numberOfMarksInCol.length == NUMBEROFSQUARESTOWIN ||
-        numberOfMarksInRow.length == NUMBEROFSQUARESTOWIN
-      ) {
-        return true;
-      }
-    }
-    return false;
-  };
-
-  const test = () => {
-    return board;
-  };
-
-  const testWinConD1 = () => {
-    board.find((square) => square.position.number == 0).mark = "x";
-    board.find((square) => square.position.number == 4).mark = "x";
-    board.find((square) => square.position.number == 8).mark = "x";
-  };
-
-  const testWinConD2 = () => {
-    board.find((square) => square.position.number == 0).mark = "x";
-    board.find((square) => square.position.number == 4).mark = "x";
-    board.find((square) => square.position.number == 8).mark = "x";
-  };
-
-  const testWinConC1 = () => {
-    board.find((square) => square.position.number == 0).mark = "x";
-    board.find((square) => square.position.number == 3).mark = "x";
-    board.find((square) => square.position.number == 6).mark = "x";
-  };
-  const testWinConR1 = () => {
-    board.find((square) => square.position.number == 0).mark = "x";
-    board.find((square) => square.position.number == 1).mark = "x";
-    board.find((square) => square.position.number == 2).mark = "x";
-  };
-
   return {
-    setMarker,
-    getAllSquaresWithDefinedMark,
-    getNumberOfEmptySquares,
-    test,
-    testWinConD1,
-    testWinConD2,
-    testWinConC1,
-    testWinConR1,
-    confirmDiagonalMarkedSquares,
-    confirmStraightMarkedSquares,
-    validateSquareIsFree,
-    getEmptySquares,
-  };
-})();
-
-const gameController = (function () {
-  let players = [];
-  let currentPlayerTurn = players[0];
-
-  const setupGame = () => {
-    const name = prompt("Pick a name");
-    const mark = prompt("Pick a token of X or O");
-    players.push(createPlayer(name, mark));
-    players[0].increaseTurnNumber();
-    players[0].increaseTurnNumber();
-    players[0].increaseTurnNumber();
-    players.push(createPlayer("Player-2", players[0].boardPiece == "X" ? "O" : "X"));
-    currentPlayerTurn = players[0];
-  };
-
-  const startTurn = (promptMessage = "Choose a sq from 0-8") => {
-    if (currentPlayerTurn == players[0]) {
-      playersChoice = prompt(promptMessage);
-    } else {
-      playersChoice = opponentController.opponentsMove();
-    }
-
-    if (!gameBoard.validateSquareIsFree(playersChoice)) {
-      startTurn("That square is already taken");
-    }
-
-    gameBoard.setMarker(playersChoice, currentPlayerTurn.boardPiece);
-    let winResult = evaluateWin(currentPlayerTurn);
-    evaluateGameEnd(winResult);
-  };
-
-  // const startPlayersTurn = (promptMessage = "Choose a sq from 0-8") => {
-  //   currentPlayerTurn = players[0];
-  //   playerOnesChoice = prompt(promptMessage);
-
-  //   if (!gameBoard.validateSquareIsFree(playerOnesChoice)) {
-  //     startPlayersTurn("That square is already taken");
-  //   }
-
-  //   gameBoard.setMarker(playerOnesChoice, players[0].boardPiece);
-  //   let winResult = evaluateWin(players[0]);
-  //   evaluateGameEnd(winResult);
-  // };
-
-  // const startOpponentsTurn = () => {
-  //   currentPlayerTurn = players[1];
-  //   playerTwosChoice = opponentController.opponentsMove();
-  //   if (!gameBoard.validateSquareIsFree(playerTwosChoice)) {
-  //     startPlayersTurn("That square is already taken");
-  //   }
-  //   gameBoard.setMarker(playerTwosChoice, players[1].boardPiece);
-  //   let winResult = evaluateWin(players[1]);
-  //   evaluateGameEnd(winResult);
-  // };
-
-  const evaluateWin = (player) => {
-    const isItTheLastTurn = gameBoard.getNumberOfEmptySquares == 0 ? true : false;
-    const marker = player.boardPiece;
-
-    if (player.getTurnNumber() < 3) {
-      return "";
-    }
-
-    if (gameBoard.confirmDiagonalMarkedSquares(marker)) {
-      return "win";
-    }
-
-    if (gameBoard.confirmStraightMarkedSquares(marker)) {
-      return "win";
-    }
-
-    if (isItTheLastTurn) {
-      return "draw";
-    } else {
-      return "";
-    }
-  };
-
-  const evaluateGameEnd = (winResult) => {
-    if (winResult == "") {
-      currentPlayerTurn = currentPlayerTurn === players[0] ? players[1] : players[0];
-      startTurn();
-    } else {
-      alert(`${winResult}`);
-    }
-  };
-
-  return {
-    evaluateWin,
     setupGame,
-    players,
+    evaluateWin,
     startTurn,
+    players,
   };
 })();
 
